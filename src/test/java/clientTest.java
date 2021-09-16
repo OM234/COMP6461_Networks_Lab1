af97c1cfe235cf66d540e5a5b92158c7ced6f7c8
Osman <frost57@gmail.com>
2021-09-16 18:46:35 -0400

    working http client in test

--- src/test/java/clientTest.java
+++ src/test/java/clientTest.java
@@ -7,18 +7,39 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class clientTest {
+//
+//    @Test
+//    void aTest() throws IOException {
+//        SocketChannel socket = SocketChannel.open();
+//        socket.connect(new InetSocketAddress("httpbin.org", 80));
+//
+//        String request = "GET /get?course=networking&assignment=1 HTTP/1.1\n" + "Host: " + "httpbin.org" +
+//                "\n" + "Accept: application/json\n\n";
+//
+//        socket.write(ByteBuffer.wrap(request.getBytes(StandardCharsets.UTF_8)));
+//        ByteBuffer bf = ByteBuffer.allocate(600);
+//        socket.read(bf);
+//        System.out.print(request);
+//        System.out.println(new String(bf.array()));
+//    }
 
     @Test
-    void aTest() throws IOException {
+    void aTest2() throws IOException {
         SocketChannel socket = SocketChannel.open();
         socket.connect(new InetSocketAddress("httpbin.org", 80));
-        socket.write(ByteBuffer.wrap("GET /status/418 HTTP/1.0".getBytes()));
-        ByteBuffer bf = ByteBuffer.allocate(100);
-        socket.read(bf);
-        System.out.println();
-    }
 
+        String request = "POST /post HTTP/1.1\n" + "Host: " + "httpbin.org"
+                + "\n" + "Content-Type: application/json\n" + "Content-Length: 17" + "\n\n" + "{\"Assignment\": 1}" ;
+        System.out.println(request);
+        socket.write(ByteBuffer.wrap(request.getBytes(StandardCharsets.UTF_8)));
+        ByteBuffer bf = ByteBuffer.allocate(600);
+        socket.read(bf);
+        System.out.println(new String(bf.array()));
+    }
 }
