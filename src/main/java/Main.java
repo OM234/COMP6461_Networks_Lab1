274007a94c75030b55ff54867fbefc4263b7176e
Oz <frost57@gmail.com>
2021-09-19 00:03:24 -0400

    moved arguments corrects to seperate package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,56 +6,34 @@ import input.helper.PostRequestParser;
 import output.HelpPresenter;
 import output.Presenter;
 
-import java.util.ArrayList;
-import java.util.List;
+import static util.InitialArgsCorrector.addInDoubleQuotes;
+import static util.InitialArgsCorrector.concatenateArgs;
 
 public class Main {
 
+    private static Application application;
+
     public static void main(String[] args) {
+        args = correctArguments(args);
+        initializeObjects(args);
+        startApplication();
+    }
+
+    private static String[] correctArguments(String[] args) {
         args = concatenateArgs(args);
         addInDoubleQuotes(args);
+        return args;
+    }
 
+    private static void initializeObjects(String[] args) {
         Parser parser = new Parser(args, new HelpRequestParser(args), new GetRequestParser(args), new PostRequestParser(args));
         HelpPresenter helpPresenter = new HelpPresenter();
         Presenter presenter = new Presenter(helpPresenter);
         HttpClient httpClient = new HttpClient();
-        Application application = new Application(parser, presenter, httpClient);
+        application = new Application(parser, presenter, httpClient);
+    }
+
+    private static void startApplication() {
         application.start();
-    }
-
-    public static String[] concatenateArgs(String args[]) {
-        List<String> newArgs = new ArrayList<>();
-        int i = 0;
-        while(i < args.length) {
-            if(args[i].contains("'")) {
-                String curr = args[i];
-                i++;
-                while(i < args.length && args[i].contains("'")) {
-                    curr += " " + args[i];
-                    i++;
-                }
-                newArgs.add(curr);
-            } else {
-                newArgs.add(args[i]);
-                i++;
-            }
-        }
-        String[] newArr = new String[newArgs.size()];
-        return newArgs.toArray(newArr);
-    }
-
-    private static void addInDoubleQuotes(String[] args) {
-        for(int i = 0; i < args.length; i++) {
-            if(args[i].contains("{")) {
-                int leftParanIndex = args[i].indexOf("{");
-                int colonIndex = args[i].indexOf(":");
-
-                String argWithQuotes = args[i].substring(0, leftParanIndex+1) + "\"";
-                argWithQuotes += args[i].substring(leftParanIndex+1, colonIndex) + "\"";
-                argWithQuotes += args[i].substring(colonIndex);
-
-                args[i] = argWithQuotes;
-            }
-        }
     }
 }
---
+++ src/main/java/util/InitialArgsCorrector.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InitialArgsCorrector {
+
+    public static String[] concatenateArgs(String args[]) {
+        List<String> newArgs = new ArrayList<>();
+        int i = 0;
+        while(i < args.length) {
+            if(args[i].contains("'")) {
+                String curr = args[i];
+                i++;
+                while(i < args.length && args[i].contains("'")) {
+                    curr += " " + args[i];
+                    i++;
+                }
+                newArgs.add(curr);
+            } else {
+                newArgs.add(args[i]);
+                i++;
+            }
+        }
+        String[] newArr = new String[newArgs.size()];
+        return newArgs.toArray(newArr);
+    }
+
+    public static void addInDoubleQuotes(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            if(args[i].contains("{")) {
+                int leftParanIndex = args[i].indexOf("{");
+                int colonIndex = args[i].indexOf(":");
+
+                String argWithQuotes = args[i].substring(0, leftParanIndex+1) + "\"";
+                argWithQuotes += args[i].substring(leftParanIndex+1, colonIndex) + "\"";
+                argWithQuotes += args[i].substring(colonIndex);
+
+                args[i] = argWithQuotes;
+            }
+        }
+    }
+}
